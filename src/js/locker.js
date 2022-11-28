a57f90b9bbc61e9767db63181b3749f45260ae99
Joy-Chang-2021 <joy50422@gmail.com>
2022-11-29 03:17:20 +0800

    add: 套件jquery、串接locker status api資料

--- src/js/locker.js
+++ src/js/locker.js
@@ -1,24 +1,48 @@
 // 套件
 import axios from 'axios';
+import $ from 'jquery';
 // 樣式
 import "../scss/locker.scss";
 
-// 使用axios套件向api取得資料
+// api
 const lockerStatusApi = "http://iotbase-gdaslocker.azurewebsites.net/webapi/system/example/box/status"
+// 儲存api資料
 let lockersData = null
 
+// 使用axios套件向api取得資料
 axios.get(lockerStatusApi)
   .then((response) => {
     // handle success
     const { data, statusText } = response
-    console.log(statusText)
+    if (statusText !== "OK") throw new Error()
     lockersData = data
-    console.log('lockersData: ', lockersData);
+    fetchLockerStatus()
   })
   .catch((error) => {
     // handle error
     console.log(error);
   })
-  .finally(() => {
-    // always executed
-  });
+
+// 根據api資料渲染畫面
+function fetchLockerStatus() {
+  // 全部智能櫃node
+  const lockerBoxes = $(".locker-boxes")
+  // 待替換的節點內容
+  let LockerHTML = ''
+
+  lockersData.Data.forEach(box => {
+    if (box.status) {
+      // status: 1 已使用
+      LockerHTML += `
+        <div class="locker-box border" data-id=${box.id} data-boxname=${box.boxname} data-status=${box.status}>已使用</div>
+      `
+    } else {
+      // status: 0 請選擇
+      LockerHTML += `
+        <div class="locker-box border available" data-id=${box.id} data-boxname=${box.boxname} data-status=${box.status}>請選擇</div>
+      `
+    }
+  })
+
+  lockerBoxes.html(LockerHTML)
+}
